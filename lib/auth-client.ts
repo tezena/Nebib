0386f6972110e48b7795ff5468789d87942dc501
yeabsira <shimelisyeabsiragithub@gmail.com>
2025-04-23 11:04:50 -0700

    deploy errors corrected

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "https://nebibforms.vercel.app", // the base url of your auth server
+  baseURL: "https://nebib-forms-9dng.vercel.app", // the base url of your auth server
 });
