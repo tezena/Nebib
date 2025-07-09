48066833a57eb4180ce3512dba540ab6c21f4754
Betelehem Jembere <betelehemjembere@gmail.com>
2025-07-09 16:40:16 +0300

    editting the url on auth-client.ys

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000", // the base url of your auth server
+  baseURL:
+    typeof window !== "undefined" && window.location.hostname !== "localhost"
+      ? "https://nebib-forms-production.up.railway.app"
+      : "http://localhost:3000", // the base url of your auth server
 });
 // .env.local
