adc5e695b927bc9258389774d844303d6464c6e3
Betelehem Jembere <betelehemjembere@gmail.com>
2025-07-09 16:53:21 +0300

    editting the url on auth-client.ys

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,6 +3,7 @@ export const authClient = createAuthClient({
   baseURL:
     typeof window !== "undefined" && window.location.hostname !== "localhost"
       ? "https://nebib-forms-production.up.railway.app"
+
       : "http://localhost:3000", // the base url of your auth server
 });
 // .env.local
